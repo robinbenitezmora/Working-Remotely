0d26524aa07774f970cf8f7c31031736576c7f07
robinbenitezmora <robinbenitez1990@gmail.com>
2023-04-03 21:45:21 -0500

    Update views

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -17,26 +17,62 @@ function PlacesList() {
   }, [])
 
   const loadingSection = (<div>Loading...</div>);
-  console.log(loadedPlaces);
-  const dataSection = loadedPlaces.map((place, index) => 
-    <div key={index}>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>City</th>
-          <th>Recent Upload Speed</th>
-          <th>Recent Upload Speed Unist</th>
-          <th>Amount of Measurements</th>
-        </tr>
-        <tr>
-          <td>{place.name}</td>
-          <td>{place.city}</td>
-          <td>{place.most_recent_download_speed}</td>
-          <td>{place.most_recent_download_units}</td>
-          <td>{place.amount_of_measurements}</td>
-        </tr>
-      </table>
-    </div>
+  const tableHeaderClass = "px-6 align-middle border border-solid py-3 text-sm uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-800"
+  const dataSection = (
+    <div>
+      <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+        <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+          <table className='min-w-full leading-normal'>
+            <thead>
+            <tr>
+                <th className={tableHeaderClass}>Name</th>
+                <th className={tableHeaderClass}>City</th>
+                <th className={tableHeaderClass}>Recent Upload Speed</th>
+                <th className={tableHeaderClass}>Recent Upload Speed Unist</th>
+                <th className={tableHeaderClass}>Amount of Measurements</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loadedPlaces.map((place, index) => {
+                return (
+                  <tr key={index}>
+                    <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                      <div className='flex items-center'>
+                        <div className='ml-3'>
+                          <p className='text-gray-900 whitespace-no-wrap'>
+                            {place.name}
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                      <p className='text-gray-900 whitespace-no-wrap'>
+                        {place.city}
+                      </p>
+                    </td>                
+                    <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                      <p className='text-gray-900 whitespace-no-wrap'>
+                        {place.most_recent_download_speed}
+                      </p>
+                    </td>
+                    <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                      <p className='text-gray-900 whitespace-no-wrap'>
+                        {place.most_recent_download_speed_unit}
+                      </p>                   
+                    </td>
+                    <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                      <p className='text-gray-900 whitespace-no-wrap'>
+                        {place.amount_of_measurements}
+                      </p>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>  
   )
 
   if (loading) {
