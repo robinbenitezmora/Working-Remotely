d21c39dfe363b3a71d156ff86bd81a727d9b2306
robinbenitezmora <robinbenitez1990@gmail.com>
2023-04-06 13:12:02 -0500

    Create initial tests

---
+++ test/controllers/api/places_controller_test.rb
@@ -0,0 +1,11 @@
+module Api
+  class PlacesControllerTest < ActiveSupport::TestCase
+    test " answers places the match search term if set" do
+      assert true
+    end
+
+    test "answers all places if search term is empty" do
+      assert false
+    end
+  end
+end
